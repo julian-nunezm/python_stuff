623f91dfb040d4162ae75ec8ebbf77ea53795006
Julian Nuñez <julian.nunezm@gmail.com>
2019-09-11 17:12:58 +1000

    Adding test for requests analysis

---
+++ readingRequests.py
@@ -0,0 +1,192 @@
+import csv, json, datetime as dt, time
+from pprint import pprint
+from tensorflow.keras.preprocessing.text import text_to_word_sequence as ttws
+csv.field_size_limit(500000)
+#https://github.com/first20hours/google-10000-english/blob/master/20k.txt
+
+commonCounter = 0
+cenitexCounter = 0
+uncommonCounter = 0
+paginationLimit = 10
+incidentsLimit = [50]   #[1000,2000,5000,10000,20000,50000,100000]
+words = 0
+
+def lookFor (incident, field):
+    cleanWords = set(ttws(incident[field])) 
+    for word in cleanWords:
+        existent = False
+        commonWordCounter = {}
+        cenitexWordCounter = {}
+        uncommonWordCounter = {}
+        #ToDo: Create also a function to look for any word to show which pool the word is in.
+        #if word != "" and word not in ["0","1","2","3","4","5","6","7","8","9","-", ">"] and "http/" not in word: #Already done by keras function
+        if word.lower() in mostCommonWords:
+            for cw in commonWords:
+                if word.lower() == cw["word"]:
+                    cw["counter"] += 1
+                    existent = True
+            if not existent:
+                commonWordCounter["word"] = word.lower()
+                commonWordCounter["counter"] = 1
+                commonWords.append(commonWordCounter)
+                global commonCounter
+                commonCounter += 1
+        elif word.lower() in commonCenitexWords:
+            for cw in cenitexWords:
+                if word.lower() == cw["word"]:
+                    cw["counter"] += 1
+                    existent = True
+            if not existent:
+                cenitexWordCounter["word"] = word.lower()
+                cenitexWordCounter["counter"] = 1
+                cenitexWords.append(cenitexWordCounter)
+                global cenitexCounter
+                cenitexCounter += 1
+        else:
+            for uw in uncommonWords:
+                if word.lower() == uw["word"]:
+                    uw["counter"] += 1
+                    existent = True
+            if not existent:
+                uncommonWordCounter["word"] = word.lower()
+                uncommonWordCounter["counter"] = 1
+                uncommonWords.append(uncommonWordCounter)
+                global uncommonCounter
+                uncommonCounter += 1
+        global words
+        words += 1
+        #else:
+            #print(word)
+
+def printCommonWords():
+    pprint(sorted(commonWords,key=lambda i: i["counter"], reverse = True))
+
+def printCenitexWords():
+    pprint(sorted(cenitexWords,key=lambda i: i["counter"], reverse = True))
+
+def printUncommonWords():
+    pprint(sorted(uncommonWords,key=lambda i: i["counter"], reverse = True))
+
+def setElapsedTime (elapsed):
+    if elapsed > 60:
+        elapsed /= 60
+        if elapsed > 60:
+            return str(round(elapsed/60,2))+" hours"
+        else:
+            return str(round(elapsed,2))+" minutes"
+    else:
+        return str(round(elapsed,2))+" seconds"
+
+try:
+    print("Start...")
+    start = time.time()
+    incidents = dict()
+    inc = dict()
+    for incLim in incidentsLimit:
+        i = 0
+        print(" ")
+        print("Beginning to analyze "+ str(incLim) +" requests...")
+        with open('requests_data.csv', 'r', encoding="utf8") as csvFile:
+            reader = csv.reader(csvFile)
+            for row in reader:
+                incident = {}
+                try:
+                    #print("Tier 1: " + row[9] + " - Number: " + row[1])
+                    if i < incLim:
+                        #print("Tier 1: " + row[9])
+                        if row[9] == "Request":
+                            print("Tier 1: " + row[9] + " - Number: " + row[1])
+                            #ToDo: Try make the analysis here to avoid more extra loops
+                            #ToDo: Check what other columns should be read!
+                            incident['Incident_Number'] = row[1]
+                            incident['Summary'] = row[4]
+                            incident['Operational_Categorization_Tier 1'] = row[9]
+                            incident['Operational_Categorization_Tier 2'] = row[10]
+                            incident['Operational_Categorization_Tier 3'] = row[11]
+                            incidents[row[1]] = incident
+                        i += 1
+                        if(i%paginationLimit==0):
+                            print(f"{i} requests loaded")
+                    else:
+                        break
+                except Exception as e1:
+                    print(f" - Error in row {i+1}: {str(e1)}")
+                    #print(type(row[25]))
+                    #print(row[25])
+                    #print(row[25][0])
+                    #print(f" -> Incident Number: {incident['Incident_Number']}, Ini Date: {incident['Reported_Date']}, End Date: {incident['Resolved_Date']}")
+                    print(f" -> Incident Number: {incident['Incident_Number']}")
+                    raise
+        csvFile.close()
+        print(f"There are {i} requests read")
+
+        """
+        inc["INC000001877013"] = incidents["INC000001877013"]
+        inc["INC000001877017"] = incidents["INC000001877017"]
+        inc["INC000001861533"] = incidents["INC000001861533"]
+        inc["INC000001940152"] = incidents["INC000001940152"]
+        print(inc["INC000001861533"])
+        print(f"Printing JSON...")
+        print(json.dumps(inc))
+        """
+
+        print(" ")
+        print("Analyzing requests...")
+        print(" ")
+
+        """
+        incident = {}
+        incident['Incident_Number'] = '12345'
+        incident['Notes'] = "This is an outage, sentence"
+        inc['12345'] = incident
+        incident = {}
+        incident['Incident_Number'] = '12346'
+        incident['Notes'] = "This is another outage at Cenitex"
+        inc['12346'] = incident
+        """
+
+        commonWordsLimit = [1]
+        for cWordsLimit in commonWordsLimit:
+            commonWords = []
+            uncommonWords = []
+            cenitexWords = []
+            commonWordCounter = {}
+            cenitexWordCounter = {}
+            uncommonWordCounter = {}
+            i = 0
+            totalWords = 0
+            print("Looking for the most common "+ str(cWordsLimit) +"k words")
+            #Loading most common words in English
+            txtFile = open(str(cWordsLimit) + 'k.txt', 'r', encoding="utf8")
+            mostCommonWords = txtFile.read().split(",")
+            txtFile.close()
+            #Loading Cenitex words
+            """txtFile = open('cenitex.txt', 'r', encoding="utf8")
+            commonCenitexWords = txtFile.read().split(",")
+            txtFile.close()"""
+            for k, v in incidents.items():    #inc or incidents
+                Here!#print("Incident Number: "+v["Incident_Number"]+". Status: "+v["Status"]+". Resolution: "+v["Resolution"])
+                #print("Incident Number: "+v["Incident_Number"]+". Status: "+v["Status"]+". Summary: "+v["Summary"]+" Notes: "+v["Notes"])
+                lookFor(v, "Summary")
+                lookFor(v, "Notes")
+                lookFor(v, "Resolution")
+                i += 1
+                if(i%paginationLimit==0):
+                    print(f"{i} incidents fields already checked")
+            print("----------------------------------------------")
+            print("Results:")
+            print("----------------------------------------------")
+            print(f" - Total words analyzed: {words}")
+            totalCounter = commonCounter + cenitexCounter + uncommonCounter
+            print(f" - Common words: {commonCounter}")
+            print(f" - Cenitex words: {cenitexCounter}")
+            print(f" - Uncommon words: {uncommonCounter}")
+            print(f" - Common words percentage: {round((commonCounter/totalCounter)*100, 2)}%")
+            print(f" - Cenitex words percentage: {round((cenitexCounter/totalCounter)*100, 2)}%")
+            print(f" - Uncommon words percentage: {round((uncommonCounter/totalCounter)*100, 2)}%")
+            print("----------------------------------------------")
+            #pprint(sorted(uncommonWords,key=lambda i: i["counter"], reverse = True))
+    print("Time: "+setElapsedTime(time.time() - start))
+    #print("Please try printCommonWords(), printCenitexWords(), or printUncommonWords if you want to see any set of words.")
+except Exception as e2:
+    print(f" - Error: {str(e2)}")
